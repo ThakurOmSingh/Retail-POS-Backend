b18cb25bf3ea34c48e9036f4584aa19f396978a7
ThakurOmSingh <105989319+ThakurOmSingh@users.noreply.github.com>
2023-10-12 17:11:06 +0530

    Update billsRoute.js Email Sending

--- routes/billsRoute.js
+++ routes/billsRoute.js
@@ -9,90 +9,80 @@ const nodemailer = require('nodemailer')
 const fs = require('fs')
 const pdfTemplate = require("../documents/documents")
 const env = require('dotenv')
+const mongoose = require('mongoose')
 env.config()
 
 
-router.post("/charge-bill",async(req,res) => {
-    try{
-        const {customerName , customerPhoneNumber , customerEmail} = req.body;
-        const newcustomer = new CustomersModel({customerName: customerName , customerPhoneNumber: customerPhoneNumber , customerEmail: customerEmail});
-        const existingCustomer = await CustomersModel.findOne({ customerPhoneNumber: customerPhoneNumber });
-if (existingCustomer) {
-    const newbill = new BillModel(req.body);
-    await newbill.save()
-    // res.send('Billing done successfully')
-} else {
-    const newcustomer = new CustomersModel({customerName: customerName , customerPhoneNumber: customerPhoneNumber , customerEmail: customerEmail});
-    await newcustomer.save()
-        const newbill = new BillModel(req.body);
-        await newbill.save()
-        // res.send('Billing done successfully')
-    
-    
-    }       
-
-    const timestamp = new Date().toISOString().replace(/[-T:.Z]/g, '');
-        const fileName = `invoice_${timestamp}.pdf`;
-
-        // Specify the folder path where you want to store the PDF files
-        const folderPath = path.join(__dirname, 'invoice');
-
-        // Create the folder if it doesn't exist
-        if (!fs.existsSync(folderPath)) {
-            fs.mkdirSync(folderPath);
-        }
-
-        const filePath = path.join(folderPath, fileName);
 
 
+const invoiceSchema = new mongoose.Schema({
+    customerName: String,
+    customerPhoneNumber: String,
+    customerEmail: String,
+    invoiceData: Buffer, 
+});
 
-        pdf.create(pdfTemplate(req.body), {}).toFile(filePath, (err) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send('Error generating PDF',err);
-            } else {
-                const pathToAttachment = path.join(__dirname, 'invoice', fileName);
-                const attachment = fs.readFileSync(pathToAttachment).toString("base64");
+const InvoiceModel = mongoose.model('Invoice', invoiceSchema);
 
-                const smtpTransport = nodemailer.createTransport({
-                    host: 'smtp.gmail.com',
-                    service: 'Gmail',
-                    port: 587,
-                    secure: false,
-                    auth: {
-                        user:process.env.USER,
-                        pass:process.env.PASSWORD
-                    },
-                    // tls: { rejectUnauthorized: false }
-                });
+router.post('/charge-bill', async (req, res) => {
+    try {
+        const { customerName, customerPhoneNumber, customerEmail } = req.body;
 
-                smtpTransport.sendMail({
-                    from: "luvkush.sitaram12345@gmail.com",
-                    to: customerEmail,
-                    subject: 'ABC store Bill',
-                    html: "<h1>Please find below the attached bill</h1>",
-                    attachments: [{
-                        content: attachment,
-                        filename: "Invoice.pdf",
-                        encoding: 'base64'
-                    }]
-                }, function (error, info) {
-                    if (error) {
-                        console.log(error);
-                        res.status(500).send('Error sending email');
-                    } else {
-                        console.log("MAIL SENT")
-                        res.status(200).send('Billing done successfully, PDF generated and email sent');
-                    }
-                });
-            }
+        
+        const pdfBuffer = await new Promise((resolve, reject) => {
+            pdf.create(pdfTemplate(req.body)).toBuffer((err, buffer) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(buffer);
+                }
+            });
         });
+
+        
+        const newInvoice = new InvoiceModel({
+            customerName,
+            customerPhoneNumber,
+            customerEmail,
+            invoiceData: pdfBuffer,
+        });
+
+        await newInvoice.save();
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.USER, 
+                pass: process.env.PASSWORD, 
+            },
+        });
+
+        
+        const mailOptions = {
+            from: "luvkush.sitaram12345@gmail.com", 
+            to: customerEmail, 
+            subject: 'Invoice', 
+            text: 'Please find attached invoice.', 
+            attachments: [
+                {
+                    filename: 'invoice.pdf', 
+                    content: pdfBuffer, 
+                },
+            ],
+        };
+
+        
+        await transporter.sendMail(mailOptions);
+
+
+        res.status(200).send('Billing done successfully, PDF generated and email sent');
     } catch (error) {
-        res.status(400).json(error);
-        console.log("error in routes");
-        console.log(error);
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-    })
+});
+
+
 
 
 
